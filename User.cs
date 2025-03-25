d1be52ca8e3afec880c2afdc398fb7dec9642953
AnhDuc228 <ducca2255@ut.edu.vn>
2025-03-25 21:48:54 +0700

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,10 @@
+namespace PregnancyTrackingBackend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Role { get; set; } = "Guest";
+    public string Email { get; set; } = string.Empty;
+}

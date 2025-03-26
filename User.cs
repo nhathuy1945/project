e73ae98c0128939ae4df957071caba0501ac9d3e
AnhDuc228 <ducca2255@ut.edu.vn>
2025-03-26 19:34:00 +0700

    Update User.cs

--- User.cs
+++ User.cs
@@ -1,10 +1,24 @@
-namespace PregnancyTrackingBackend.Models;
+using System.ComponentModel.DataAnnotations;
 
-public class User
+namespace PregnancyTrackingBackend.Models
 {
-    public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string Role { get; set; } = "Guest";
-    public string Email { get; set; } = string.Empty;
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Role is required")]
+        public string Role { get; set; }
+    }
 }

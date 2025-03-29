24add052f08f73374d3abe805a8bec2871e7f479
AnhDuc228 <ducca2255@ut.edu.vn>
2025-03-29 17:10:47 +0700

    Delete pregnancy-tracker directory

--- pregnancy-tracker/src/main/java/com/babycenter/pregnancy_tracker/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.babycenter.pregnancy_tracker.repository;
-
-import com.babycenter.pregnancy_tracker.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}

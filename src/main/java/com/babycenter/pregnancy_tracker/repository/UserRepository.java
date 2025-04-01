944f254a001fed944f244be4d47e26c5d41e1c1b
nhathuy1945 <nhathuy190405@gmail.com>
2025-04-01 14:00:17 +0700

    UserRepository.java

---
+++ src/main/java/com/babycenter/pregnancy_tracker/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.babycenter.pregnancy_tracker.repository;
+
+import com.babycenter.pregnancy_tracker.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}

99d57b65653b4cc8ce5bf15a8bc2bd9023eeda14
Anhduc228 <ducca2255@ut.edu.vn>
2025-04-08 20:38:02 +0700

    up HealthAdviceResponse + WeatherResponse (Model)

---
+++ src/main/java/com/babycenter/pregnancy_tracker/model/WeatherResponse.java
@@ -0,0 +1,44 @@
+package com.babycenter.pregnancy_tracker.model;
+
+public class WeatherResponse {
+
+    private String name;
+    private Main main;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public static class Main {
+        private double temp;
+        private int humidity;
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
+
+        public int getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(int humidity) {
+            this.humidity = humidity;
+        }
+    }
+}
